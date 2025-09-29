e27a4cb70e554bd4040b5e9002e42191c03b5284
Bill Mårtensson <bill@magictechnology.se>
2025-09-29 12:44:41 +0200

    layout

---
+++ app/FancyBox.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+
+export function FancyBox() {
+    return (
+        <View style={{
+            backgroundColor: "gray",
+            width: 120,
+            height: 100
+        }}>
+            <Text>HEJ</Text>
+        </View>
+    )
+}
---
+++ app/index.tsx
@@ -0,0 +1,55 @@
+import { StyleSheet, Text, View } from "react-native";
+import { FancyBox } from "./FancyBox";
+
+export default function Index() {
+    return (
+        <View
+            style={{
+                flex: 1,
+                justifyContent: "space-between",
+                alignItems: "center",
+                flexDirection: "column",
+                backgroundColor: "blue"
+            }}
+        >
+            <View style={styles.redbox}>
+                <View style={{
+                    backgroundColor: "green",
+                    width: 70,
+                    height: 70,
+                    marginLeft: 30
+                }}>
+                </View>
+
+                <Text style={{
+                    flex: 1,
+                    textAlign: "center",
+                    fontSize: 20
+                }}>RUBRIK</Text>
+            </View>
+
+            <FancyBox />
+            <FancyBox />
+
+            <View
+                style={{
+                    backgroundColor: "yellow",
+                    width: 200,
+                    height: 70,
+                    marginBottom: 50
+                }}>
+            </View>
+
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    redbox: {
+        flexDirection: "row",
+        backgroundColor: "red",
+        height: 120,
+        width: "100%",
+        alignItems: "center"
+    }
+})
